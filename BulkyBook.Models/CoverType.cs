194143ce551866e141727d12f1626418f5b2cd0a
Lesedi Seemane <lseemane@outlook.com>
2025-02-02 22:50:48 +0200

    CRUD and views

--- BulkyBook.Models/CoverType.cs
+++ BulkyBook.Models/CoverType.cs
@@ -12,7 +12,7 @@ namespace BulkyBook.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        [Display(Name = "CoverType")]
+        [Display(Name = "Cover Type")]
         [MaxLength(50)]
         public string Name { get; set; }
     }
