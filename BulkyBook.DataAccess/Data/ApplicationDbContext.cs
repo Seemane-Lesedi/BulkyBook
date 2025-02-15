f1dcca161bb59e5572071833d3074408f7070401
Lesedi Seemane <lseemane@outlook.com>
2025-02-15 20:05:49 +0200

    Create product model and add product to database

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -15,6 +15,7 @@ namespace BulkyBook.DataAccess
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
  
